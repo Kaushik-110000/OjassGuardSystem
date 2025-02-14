85c3da1ce003d458d56b0bbd7dc6488743bfe13d
Kaushik-110000 <abhishekkumar89647@gmail.com>
2025-02-14 16:53:49 +0530

    map

--- newFrontend/src/components/Map.jsx
+++ newFrontend/src/components/Map.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function LocationMarker({ onLocationSelect }) {
+  
   const [position, setPosition] = useState(null);
 
   // Capture user click and update marker position
@@ -17,7 +24,9 @@ function LocationMarker({ onLocationSelect }) {
 
   return position === null ? null : (
     <Marker position={position}>
-      <Popup>Latitude: {position[0]}, Longitude: {position[1]}</Popup>
+      <Popup>
+        Latitude: {position[0]}, Longitude: {position[1]}
+      </Popup>
     </Marker>
   );
 }
@@ -29,7 +38,11 @@ function Map() {
 
   return (
     <div style={{ height: "500px", width: "100%" }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100%", width: "100%" }}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
         {/* OpenStreetMap Tile Layer */}
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
